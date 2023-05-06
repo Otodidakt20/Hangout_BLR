dbb86a64d5f38b9b42e7d71c7c6bf3aeb43db86f
Anishi Raj <raj.anishi24nov123@gmail.com>
2023-05-06 11:12:53 +0530

    new file:   index.html new file:   modules/app.js new file:   package.json modified:   script.js new file:   styles.css

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const express = require('express');
+let loc_1 = ["lat_1", "long_1"];
+let loc_2 = ["lat_2", "long_2"];
+function getMidpoint(loc_1, loc_2) {
+  midpoint_lat = (loc_2[0] - loc_1[0]) / 2;
+  midpoint_long = (loc_2[1] - loc_1[1]) / 2;
+  return [midpoint_lat, midpoint_long];
+}
+
+console.log(getMidpoint(loc_1, loc_2));
